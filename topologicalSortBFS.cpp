5d63c2394686dcd7065ed074cceae36ba00bf71e
abhishekbali1510 <abhishekbali15oct@gmail.com>
2022-07-05 13:09:50 +0530

    topological sort using BFS

---
+++ topologicalSortBFS.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void input(vector<int> arr[],int n,int m)
+{
+    for(int i=1;i<=m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        arr[u].push_back(v);
+        // arr[v].push_back(u); DAG
+    }
+}
+
+queue<int>q;
+
+void topoBFS(vector<int>arr[],vector<int>indeg,int n)
+{
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<arr[i].size();j++)
+        {
+            indeg[arr[i][j]]++;
+        }
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        if(indeg[i]==0)
+            q.push(i);
+    }
+
+    while(!q.empty())
+    {
+        int tmp=q.front();
+        q.pop();
+        cout<<tmp<<" ";
+        for(int i=0;i<arr[tmp].size();i++)
+        {
+            indeg[arr[tmp][i]]--;
+            if(indeg[arr[tmp][i]]==0)
+                q.push(arr[tmp][i]);
+        }
+    }
+}
+
+int main()
+{
+    int n,m;
+    cout<<"Enter number of nodes and edges : ";
+    cin>>n>>m;
+    vector<int> arr[n+1];
+    
+    cout<<"Enter the edges : \n";
+    input(arr,n,m); //arr is our list
+
+    vector<int> indeg(n+1,0);
+    
+    topoBFS(arr,indeg,n);
+
+}
